eb56055c68b34de4339334c01fd6c71c501747af
Eugene <>
2023-03-25 18:14:28 +0100

    задание 2

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿//Задание 2
+Console.WriteLine("Введите число один");
+string OneNum = Console.ReadLine();
+int One = Convert.ToInt32(OneNum);
+
+Console.WriteLine("Введите число два");
+string TwoNum = Console.ReadLine();
+int Two = Convert.ToInt32(TwoNum);
+
+if (One > Two) {
+    Console.WriteLine($"Большее число равно: {One}");
+}
+else {
+    Console.WriteLine($"Большее число равно: {Two}");
+}
